719ca89a25069578ee8a6082a8391375b1f7d125
BILLET ANAEC <abillet001@scidesse087.univ-pau.fr>
2019-09-12 09:04:12 +0200

    updated bigmemory code

--- codebigmemory.R
+++ codebigmemory.R
@@ -22,3 +22,14 @@ summary(dataX[,1:4])
 
 #Range of column 1
 range(dataX[,1])
+
+#we might be interested in customer 2442’s
+#movies which were rated 2 or worse during February through October of 2004
+these <- mwhich(dataX, c("customer", "year", "month", "rating"), list(2442, 2004,
+                                                                  c(2, 10), 2), list("eq", "eq", c("ge", "le"), "le"), "AND")
+dataX[these, ]
+
+#We provide the movie titles to place these ratings in context
+mnames <- read.csv("movie_titles.csv", header = FALSE)
+names(mnames) <- c("movie", "year", "Name of Movie")
+mnames[mnames[, 1] %in% unique(dataX[these, 1]), c(1, 3)]
