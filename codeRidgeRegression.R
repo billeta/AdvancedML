4128cf904e706f0ff1f268ad64eac390f55d7639
BILLET ANAEC <abillet001@scidesse087.univ-pau.fr>
2019-09-18 15:26:13 +0200

    Ridge updated

--- codeRidgeRegression.R
+++ codeRidgeRegression.R
@@ -1,3 +1,5 @@
+#####LINEAR REGRESSION##
+
 #Creating a 20*10 matrix
 matX <- matrix(rnorm(20*10),ncol=20,nrow=10)
 
@@ -13,6 +15,8 @@ mod <- lm(y~matX)
 summary(mod)
 #As we can expect the normal linear regression failed because too much beta
 
+#####RIDGE REGRESSION#####
+
 #Let's try the ridge regression
 library(MASS)
 lm.ridge(y~matX-1,lambda=0.1) #The -1 mean that we try to explain y without an intercept
@@ -20,4 +24,45 @@ lm.ridge(y~matX-1,lambda=0.1) #The -1 mean that we try to explain y without an i
 #we can do it with glmnet library as well
 library(glmnet)
 model <- glmnet(matX,y,alpha=0,lambda = 0.1) #lambda is the tuning parameter and we want to find the best one (use cross validation)
+
+#this is the beta of the model
 as.vector(model$beta)
+
+
+#Lets see how ridge regression fails with correlation between variables
+set.seed(10)
+#generating correlated variables
+x1 <- rnorm(20)
+x2 <- rnorm(20,mean=x1,sd=.01)
+#Checking correlation
+cor(x1,x2)
+
+#Creating the model
+y <- 3+x1+x2+rnorm(20)
+#Run the model
+lm(y~x1+x2)$coef
+
+#Checking the estimator
+lm.ridge(y~x1+x2,lambda=1)
+#!!!!! Careful correlated data give back bad estimator the x1 and x2 harshly different!
+
+
+#lets try with the breath cancer dataset
+load("bcTCGA.RData")
+#Checking the dim of the dataset
+dim(X)
+
+#We try to find the best beta to choose using a cross validation (10 folds by default)
+tune <- cv.glmnet(X,y,alpha=0)
+plot(tune)
+
+#performing ridge regretion using the best value of beta
+ridge <- glmnet(X,y,lambda=tune$lambda.1se,alpha=0)
+#looking at our new parameters
+head(ridge$beta)
+
+#Checking how many parameters are relevant (>0.003)
+length(which(abs(ridge$beta[,1])>0.003))
+
+#Computing the MSEP
+mean((predict(ridge,newx = X)-y)**2)
